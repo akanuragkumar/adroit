b0a09735d09c94ac22195f318e3fdecbe611bb7c
rajsingh12497 <rajsingh12497@gmail.com>
2018-04-01 23:32:13 +0530

    Add files via upload
    demo

---
+++ demo.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "1234";
+$dbname = "Data";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO Info (Name, Phone)
+VALUES ('Gaurav', '913')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
